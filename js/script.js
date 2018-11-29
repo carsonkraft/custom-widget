33c8cff16cf7763432dad0c2f246a60c0a118902
carsonkraft <carsonk200@gmail.com>
2018-11-29 14:42:09 -0500

    pretty much works

--- js/script.js
+++ js/script.js
@@ -1,16 +1,14 @@
   $.widget( "custom.mySlider", $.ui.slider, {
-
       tabs: function() {
           var max = this.options.max;
-          for(i = 0; i < max; i++){
-            this.element.append('<div class="container" id="tab'+ i +'"></div>');
+          for(i = 0; i <= max; i++){
+            this.element.append('<div class="tabcontent" id="tab'+ i +'"></div>');
           }
       },
       addTabContent: function(tabContents, tabNumber) {
           var max = this.options.max;
             $( '#tab'+ tabNumber ).append(tabContents);
-      }
-
+      },
   });
 
 
@@ -18,14 +16,23 @@
       $( "#slider2" ).mySlider({
         value: 0,
         min: 0,
-        max: 5,
+        max: 3,
         step: 1,
         range: "min",
         slide: function( event, ui ) {
           $( "#amount" ).val( ui.value );
+          var tabValue = ui.value;
+          var tabcontent = document.getElementsByClassName("tabcontent");
+          for (i = 0; i < tabcontent.length; i++) {
+              tabcontent[i].style.display = "none";
+          }
+          document.getElementById("tab" + tabValue).style.display = "block";
         }
       });
       $( "#amount" ).val( $( "#slider2" ).mySlider( "value" ) );
       $( "#slider2" ).mySlider( "tabs");
-      $( "#slider2" ).mySlider( "addTabContent","hello world",2);
+      $( "#slider2" ).mySlider( "addTabContent","Review Your Order",2);
+      $( "#slider2" ).mySlider( "addTabContent","Billing Info",1);
+      $( "#slider2" ).mySlider( "addTabContent","Shipping Info",0);
+      $( "#slider2" ).mySlider( "addTabContent","Thank you for your order!",3);
     } );
